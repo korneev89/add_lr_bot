04d85a1013aa7dcedaf9c47823dad0dbb59abb95
Dmitry Korneev <31965644+korneev89@users.noreply.github.com>
2018-10-09 14:44:55 +0300

    second one

---
+++ leave_request_bot/CalendarEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TlgrmBot
+{
+	public class CalendarEvent
+	{
+		public string Name { get; set; }
+		public string Date { get; set; }
+		public int Start { get; set; }
+		public int End { get; set; }
+		public int FromTlgrmID { get; set; }
+
+		public CalendarEvent()
+		{
+			Name = "";
+			Date = "";
+			Start = 0;
+			End = 0;
+		}
+
+		static public void Clear(CalendarEvent ev)
+		{
+			ev.Name = "";
+			ev.Date = "";
+			ev.Start = 0;
+			ev.End = 0;
+		}
+	}
+}
